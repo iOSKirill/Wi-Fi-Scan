8f213b2f6d9d16e77124f487e338e24559dbebac
Kirill Manuilenko <m.kirill4163@gmail.com>
2024-09-19 16:33:35 +0300

    Initial commit #2

---
+++ WiFiScan/WiFiScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WiFiScan
+//
+//  Created by Kirill Manuilenko on 19.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WiFiScan/WiFiScan/WiFiScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiScanApp.swift
+//  WiFiScan
+//
+//  Created by Kirill Manuilenko on 19.09.24.
+//
+
+import SwiftUI
+
+@main
+struct WiFiScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
